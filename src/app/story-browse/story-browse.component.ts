eff2d436f22b5d7db7bd6e476e8703adc12219f7
Georgescu Alexandra Elena <alestef81@gmail.com>
2024-06-18 13:07:07 +0300

    commit

--- src/app/story-browse/story-browse.component.ts
+++ src/app/story-browse/story-browse.component.ts
@@ -9,61 +9,84 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-    standalone: true,
-    imports: [
-        RouterModule,
-        HttpClientModule,
-        CommonModule,
-        FormsModule,
-        ReactiveFormsModule
-    ],
-    selector: 'app-story-browse',
-    templateUrl: './story-browse.component.html',
-    styleUrls: ['./story-browse.component.css'],
-    providers: [DataService]
+  standalone: true,
+  imports: [
+    RouterModule,
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  selector: 'app-story-browse',
+  templateUrl: './story-browse.component.html',
+  styleUrls: ['./story-browse.component.css'],
+  providers: [DataService],
 })
 export class StoryBrowseComponent implements OnInit {
-    stories: StoryModel[] = [];
-    pageSize: number;
-    pageId: number;
-    prevBtnDisabled: boolean = false;
-    nextBtnDisabled: boolean = false;
-    selectedGenre: string = '';
-    genres: string[] = ['SF', 'Fantasy', 'Mystery', 'Romance', 'Horror', 'Adventure', 'Comedy'];
+  stories: StoryModel[] = [];
+  pageSize: number;
+  pageId: number;
+  prevBtnDisabled: boolean = false;
+  nextBtnDisabled: boolean = false;
+  selectedGenre: string = '';
+  genres: string[] = [];
 
-    constructor(private sanitizer: DomSanitizer, private titleService: Title, private router: Router, private route: ActivatedRoute, private dataService: DataService, public utils: UtilsService) {
-        this.titleService.setTitle('Browse Stories');
-        this.router.routeReuseStrategy.shouldReuseRoute = () => false;
-        this.pageSize = +this.route.snapshot.params['pageSize'] || 10;
-        this.pageId = +this.route.snapshot.params['pageId'] || 0;
-        
-        this.fetchStories();
-    }
+  constructor(
+    private sanitizer: DomSanitizer,
+    private titleService: Title,
+    private router: Router,
+    private route: ActivatedRoute,
+    private dataService: DataService,
+    public utils: UtilsService
+  ) {
+    this.titleService.setTitle('Browse Stories');
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.pageSize = +this.route.snapshot.params['pageSize'] || 10;
+    this.pageId = +this.route.snapshot.params['pageId'] || 0;
 
-    ngOnInit() {}
+    this.fetchStories();
+    this.fetchGenres();
+  }
 
-    fetchStories() {
-        const genreQuery = this.selectedGenre ? `?genre=${this.selectedGenre}` : '';
-        this.dataService.get<StoryModel[]>('story/browse/' + this.pageSize + '/' + this.pageId + genreQuery, serverStories => {
-            this.stories = serverStories;
-            this.prevBtnDisabled = this.pageId <= 0;
-            this.nextBtnDisabled = this.stories.length < this.pageSize;
-        }, error => {
-            this.utils.showMessage('There was a problem!');
-            console.log(`Error response: ${error}`);
-        });
-    }
+  ngOnInit() {}
 
-    filterStories() {
-        this.pageId = 0; 
-        this.fetchStories();
-    }
+  fetchGenres() {
+    this.dataService.get<string[]>('story/genres', genres => {
+        this.genres = genres;
+    }, error => {
+        this.utils.showMessage('There was a problem fetching genres!');
+        console.log(`Error response: ${error}`);
+    });
+}
 
-    getImageUrl(relativeUrl?: string): string {
-        return relativeUrl ? `http://localhost:50295/${relativeUrl}` : 'http://localhost:50295/StaticFiles/Images/standard.jpg';
-    }
+  fetchStories() {
+    const genreQuery = this.selectedGenre ? `?genre=${this.selectedGenre}` : '';
+    this.dataService.get<StoryModel[]>(
+      'story/browse/' + this.pageSize + '/' + this.pageId + genreQuery,
+      (serverStories) => {
+        this.stories = serverStories;
+        this.prevBtnDisabled = this.pageId <= 0;
+        this.nextBtnDisabled = this.stories.length < this.pageSize;
+      },
+      (error) => {
+        this.utils.showMessage('There was a problem!');
+        console.log(`Error response: ${error}`);
+      }
+    );
+  }
 
-    sanitizeContent(content: string): SafeHtml {
-        return this.sanitizer.bypassSecurityTrustHtml(content);
-    }
+  filterStories() {
+    this.pageId = 0;
+    this.fetchStories();
+  }
+
+  getImageUrl(relativeUrl?: string): string {
+    return relativeUrl
+      ? `http://localhost:50295/${relativeUrl}`
+      : 'http://localhost:50295/StaticFiles/Images/standard.jpg';
+  }
+
+  sanitizeContent(content: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(content);
+  }
 }
