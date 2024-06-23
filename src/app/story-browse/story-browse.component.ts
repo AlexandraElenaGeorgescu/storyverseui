e318e9f50a58f201c28ff16fb9313b24d8ab10c2
Georgescu Alexandra Elena <alestef81@gmail.com>
2024-06-23 15:10:34 +0300

    commit

--- src/app/story-browse/story-browse.component.ts
+++ src/app/story-browse/story-browse.component.ts
@@ -30,6 +30,24 @@ export class StoryBrowseComponent implements OnInit {
   nextBtnDisabled: boolean = false;
   selectedGenre: string = '';
   genres: string[] = [];
+  sortBy: string = '';
+
+  sortOptions: { key: string, value: string }[] = [
+    { key: 'authorDescending', value: 'Author (Z-A)'},
+    { key: 'authorAscending', value: 'Author (A-Z)' },
+    { key: 'nameDescending', value: 'Title (Z-A)' },
+    { key: 'nameAscending', value: 'Title (A-Z)' },
+    { key: 'dateCreatedDescending', value: 'Descending by Date' },
+    { key: 'dateCreatedAscending', value: 'Ascending by Date' },
+    { key: 'reviewCountDescending', value: 'Reviews High to Low' },
+    { key: 'reviewCountAscending', value: 'Reviews Low to High' },
+    { key: 'bookmarksCountDescending', value: 'Bookmarks High to Low' },
+    { key: 'bookmarksCountAscending', value: 'Bookmarks Low to High' },
+    { key: 'subscribersCountDescending', value: 'Subscribers High to Low' },
+    { key: 'subscribersCountAscending', value: 'Subscribers Low to High' },
+    { key: 'averageRatingDescending', value: 'Best Stories High to Low' },
+    { key: 'averageRatingAscending', value: 'Best Stories Low to High' }
+  ];
 
   constructor(
     private sanitizer: DomSanitizer,
@@ -43,6 +61,8 @@ export class StoryBrowseComponent implements OnInit {
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
     this.pageSize = +this.route.snapshot.params['pageSize'] || 10;
     this.pageId = +this.route.snapshot.params['pageId'] || 0;
+    this.selectedGenre = this.route.snapshot.queryParams['genre'] || '';
+    this.sortBy = this.route.snapshot.queryParams['sortBy'] || '';
 
     this.fetchStories();
     this.fetchGenres();
@@ -59,24 +79,34 @@ export class StoryBrowseComponent implements OnInit {
     });
 }
 
-  fetchStories() {
-    const genreQuery = this.selectedGenre ? `?genre=${this.selectedGenre}` : '';
-    this.dataService.get<StoryModel[]>(
-      'story/browse/' + this.pageSize + '/' + this.pageId + genreQuery,
-      (serverStories) => {
-        this.stories = serverStories;
-        this.prevBtnDisabled = this.pageId <= 0;
-        this.nextBtnDisabled = this.stories.length < this.pageSize;
-      },
-      (error) => {
-        this.utils.showMessage('There was a problem!');
-        console.log(`Error response: ${error}`);
-      }
-    );
-  }
+fetchStories() {
+  const genreQuery = this.selectedGenre ? `genre=${this.selectedGenre}&` : '';
+  const sortQuery = `sortBy=${this.sortBy}`;
+  const query = `story/browse/${this.pageSize}/${this.pageId}?${genreQuery}${sortQuery}`;
+  this.dataService.get<StoryModel[]>(
+    query,
+    (serverStories) => {
+      this.stories = serverStories;
+      this.prevBtnDisabled = this.pageId <= 0;
+      this.nextBtnDisabled = this.stories.length < this.pageSize;
+    },
+    (error) => {
+      this.utils.showMessage('There was a problem!');
+      console.log(`Error response: ${error}`);
+    }
+  );
+}
 
   filterStories() {
     this.pageId = 0;
+    this.router.navigate(['/story-browse', this.pageSize, this.pageId], { queryParams: { genre: this.selectedGenre, sortBy: this.sortBy } });
+    this.fetchStories();
+  }
+
+  updateSort(sortBy: string) {
+    this.sortBy = sortBy;
+    this.pageId = 0;
+    this.router.navigate(['/story-browse', this.pageSize, this.pageId], { queryParams: { genre: this.selectedGenre, sortBy: this.sortBy } });
     this.fetchStories();
   }
 
